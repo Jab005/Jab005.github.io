566309331a80f0b74a9621eb1e3a260eea4a51ca
Jab <164631211+Jab005@users.noreply.github.com>
2024-12-29 18:31:09 +0700

    New Upd

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const CurrentTime = new Date();
+const thisYear = CurrentTime.getFullYear();
+const thisMonth = CurrentTime.getMonth()+1;//Month started from 0 somehow..
+const thisDate = CurrentTime.getDate();
+const rgp = document.querySelector(".prof");
+const dmcage = document.querySelector(".dmcage");
+
 var musicmd = false;
 var imgsh = ["ctrimg01.jpg","ctrimg02.jpg","ctrimg03.jpg","ctrimg04.jpg","ctrimg05.jpg"];
+var age = ((thisMonth>=11)&&(thisDate>=10)) ? thisYear-2008 : thisYear-2009;
 
-const rgp = document.querySelector(".prof");
-
-//func copy #2
 function randInt(min=0,max=Number.MAX_SAFE_INTEGER){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -11,6 +16,7 @@ function menuBtn(){
   var menu = document.querySelector(".menubar");
   var arw = document.querySelector(".menuarw");
   menu.style.width = menu.style.width=="300px" ? "0px" : "300px";
+  menu.style.borderRight = menu.style.width=="300px" ? "2px solid white" : "0px solid white";
   arw.style.transform = menu.style.width=="300px" ? "translateX(-50%)rotate(90deg)" : "translateX(-50%)rotate(-90deg)";
 };
 function musicSet(){
@@ -21,11 +27,16 @@ function musicSet(){
   musicicon.style.transform = musicmd ? "rotate(360deg)" : "rotate(0deg)";
   musicmd ? musicbg.play() : musicbg.pause();
 };
+function shuffleProf(){
+  rgp.src = "assets/"+imgsh[randInt(0,imgsh.length-1)];
+};
 
-rgp.src = "assets/"+imgsh[randInt(0,imgsh.length-1)];
 rgp.addEventListener("click", () => {
   rgp.style.borderRadius = "0px";
   setTimeout(() => {
     rgp.style.borderRadius = "50%";
   },1500);
 });
+
+rgp.src = "assets/"+imgsh[randInt(0,imgsh.length-1)];
+dmcage.textContent = `I'm from Indonesia. Born on November 2008 (${age} Years old).`;
